bb06fdb7e8ace8452d45499d3e9b7dcee86f9da8
RCG10lite <105557520+RCG10Lite@users.noreply.github.com>
2022-06-27 10:09:04 -0700

    Add files via upload

--- Tiny Tool Launcher.bat
+++ Tiny Tool Launcher.bat
@@ -6,7 +6,8 @@ title Wait 5 Seconds
 echo.                    ___________________________________________________________
 Echo.                    ^|                                                          ^|
 Echo.                    ^|   Dont rename or delete any Files or the Script          ^|
-Echo.                    ^|   wont Function                                          ^|
+Echo.                    ^|   wont Function.                                         ^|
+Echo.                    ^|   Dont run script from WinRAR or Temp Directories.       ^|
 Echo.                    ^|                                                          ^|
 Echo.                    ^|   In 5 Seconds Script will Start                         ^|
 Echo.                    ^|__________________________________________________________^|
